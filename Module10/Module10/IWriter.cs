800b055a1a396c0b039d5661974b31e21d2ecc3a
HelenArkh <alena.arkh@gmail.com>
2021-02-01 21:29:47 +0300

    Множественная реализация интерфейсов

--- Module10/Module10/IWriter.cs
+++ Module10/Module10/IWriter.cs
@@ -9,6 +9,34 @@ namespace Module10
         void Write();
     }
 
+    public interface IReader
+    {
+        void Read();
+    }
+
+    public interface IMailer
+    {
+        void SendEmail();
+    }
+
+    public class FileManager : IWriter, IReader, IMailer
+    {
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class Writer : IWriter
     {
         void IWriter.Write()
