d1f6b94669b280cf442f6d8647a429d182c9ec8c
HelenArkh <alena.arkh@gmail.com>
2021-02-01 20:48:13 +0300

    Создала проект и изучила интерфейсы

---
+++ Module10/Module10/IWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module10
+{
+    public interface IWriter
+    {
+        void Write() { Console.WriteLine("Hello, I'm Writer!"); }
+    }
+
+    public class Writer : IWriter
+    {
+
+    }
+}
